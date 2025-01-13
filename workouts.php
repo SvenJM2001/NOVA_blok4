b93f8a22f9fc681ab1956f0e9205bf45f8e0e1fd
Sven <sven.moorman@gmail.com>
2025-01-13 17:00:24 +0100

    paginas toegevoegd met nav

--- workouts.php
+++ workouts.php
@@ -7,15 +7,15 @@ include "connection.php"
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Work4Me</title>
+    <title>Work4Me_workouts</title>
     <link rel="stylesheet" href="stylesheet.css">
 </head>
 <body>
     <header>
         <nav>
             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="workouts.php">Workouts</a></li>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Workouts</a></li>
                 <li><a href="data.php">Data</a></li>
                 <li><a class = "register" href="register.php">Registreren</a></li>
             </ul>
