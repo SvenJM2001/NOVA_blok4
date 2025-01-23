affcaa3f391ac4833c70dcf6c49d90c8e31eb1ef
intConvict <joostvangent911@gmail.com>
2025-01-23 14:35:07 +0100

    header login/loguit

--- header.php
+++ header.php
@@ -8,25 +8,37 @@ session_start();
     <ul>
       <li><a class="nav" href="workouts.php">Workouts</a></li>
       <li><a class="nav" href="data.php">Data</a></li>
-      <?php
-      if(isset($_SESSION['gebruikersnaam'])){
-      ?>
-      <li><a href="profiel.php"><?php echo $_SESSION['gebruikersnaam'] ?></a></li>
-      <?php
-      } else {
-      ?>
-      <li><a href="login.php">Inloggen</a></li>
-      <?php
-      }
-      ?>
-  </ul>
-  <div class="dropdown">
-    <button class="dropdown_button">Mijn account</button>
-    <ul class="dropdown_content">
-        <li><a href="#">Mijn gegevens</a></li>
-        <li><button id="login_button" onclick="window.location.href = 'login.php';">Login</button></li>
-        <li><button id="loguit_button" onclick="window.location.href = 'loguit.php';">Log uit</button></li>
-      </ul>
-    </div>
+      <li>
+        <div class="dropdown">
+          <button class="dropdown_button">
+            <?php
+              if(isset($_SESSION['gebruikersnaam'])){
+            ?>
+            <a href="profiel.php"><?php echo $_SESSION['gebruikersnaam'] ?></a>
+            <?php
+              } else {
+            ?>
+            <a href="login.php">Inloggen</a>
+            <?php
+              }
+            ?>
+          </button>
+          <ul class="dropdown_content">
+            <li><a href="#">Mijn gegevens</a></li>
+            <?php
+              if(isset($_SESSION['gebruikersnaam'])){
+            ?>
+            <li><button id="loguit_button" onclick="window.location.href = 'loguit.php';">Log uit</button></li>
+            <?php
+              } else {
+            ?>
+            <li><button id="login_button" onclick="window.location.href = 'login.php';">Login</button></li>
+            <?php
+              }
+            ?>
+          </ul>
+        </div>
+      </li>
+    </ul>
   </nav>
 </header>
